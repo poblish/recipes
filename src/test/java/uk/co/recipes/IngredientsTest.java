943db6805edf91f4c8d93894a7e0385eda05fe0f
Andrew Regan <aregan@gmail.com>
2013-06-19 00:03:43 +0100

    Start to test JSON persistence

--- src/test/java/uk/co/recipes/IngredientsTest.java
+++ src/test/java/uk/co/recipes/IngredientsTest.java
@@ -5,8 +5,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
+import java.net.URL;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import uk.co.recipes.api.CommonTags;
@@ -16,19 +29,70 @@ import uk.co.recipes.api.ITag;
 import uk.co.recipes.api.Units;
 
 import com.google.common.base.Optional;
+import com.google.common.base.Throwables;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.InstanceCreator;
 
 public class IngredientsTest {
 
+	private final static HttpClient CLIENT = new DefaultHttpClient();
+
 	private final static Gson GSON = builder().create();
+	private final static ObjectMapper MAPPER = new ObjectMapper();
+
+//	private final static Charset UTF8 = Charset.forName("UTF-8");
+
+	private final static String	IDX_URL = "http://localhost:9200/recipe/items";
+
+
+	@BeforeClass
+	public void cleanIndices() throws ClientProtocolException, IOException {
+		final HttpResponse resp = CLIENT.execute( new HttpDelete(IDX_URL) );
+		EntityUtils.consume( resp.getEntity() );
+
+		final ICanonicalItem lamb = new CanonicalItem("Lamb");
+		final ICanonicalItem lambNeck = new CanonicalItem("Lamb Neck", lamb);
+
+		put(lamb, "1");
+		put(lambNeck, "2");
+	}
+
+	public void put( final ICanonicalItem inItem, String inId) throws IOException {
+		final HttpPost req = new HttpPost( IDX_URL + "/" + inId);
+
+		try {
+			req.setEntity( new StringEntity( GSON.toJson(inItem) ) );
+//			String s = MAPPER.writeValueAsString(inItem);
+//			req.setEntity( new StringEntity( MAPPER.writeValueAsString(inItem) ) );
+
+			final HttpResponse resp = CLIENT.execute(req);
+			assertThat( resp.getStatusLine().getStatusCode(), is(201));
+			EntityUtils.consume( resp.getEntity() );
+		}
+		catch (UnsupportedEncodingException e) {
+			Throwables.propagate(e);
+		}
+	}
+
+	public ICanonicalItem get( final ICanonicalItem inItem, String inId) throws IOException {
+		return MAPPER.readValue( MAPPER.readTree( new URL( IDX_URL + "/" + inId) ).path("_source"), CanonicalItem.class);
+//		final HttpResponse resp = CLIENT.execute( new HttpGet( IDX_URL + "/1") );
+//
+//		try {
+//			final byte[] bs = ByteStreams.toByteArray( resp.getEntity().getContent() );
+//			return GSON.fromJson( new String( bs, UTF8), CanonicalItem.class);
+//		}
+//		finally {
+//			EntityUtils.consume( resp.getEntity() );
+//		}
+	}
 
 	// See: http://www.bbcgoodfood.com/recipes/5533/herby-lamb-cobbler
 	@Test
-	public void initialTest() {
-		final ICanonicalItem lamb = new CanonicalItem("Lamb");
-		final ICanonicalItem lambNeck = new CanonicalItem("Lamb Neck", lamb);
+	public void initialTest() throws IOException {
+		final ICanonicalItem lamb = get( null, "1");
+		final ICanonicalItem lambNeck = get( null, "2"); // new CanonicalItem("Lamb Neck", lamb);
 
 		final Ingredient lambIngredient = new Ingredient( new NamedItem(lambNeck), new Quantity( Units.GRAMMES, 900));
 		lambIngredient.addNote( ENGLISH, "Neck fillets, cut into large chunks");
@@ -55,7 +119,7 @@ public class IngredientsTest {
 
 		final String recipeJson = GSON.toJson(r);
 		System.out.println(recipeJson);
-		assertThat( recipeJson, is("{\"stages\":[{\"ingredients\":[{\"item\":{\"name\":\"Lamb Neck\",\"canonicalItem\":{\"canonicalName\":\"Lamb Neck\",\"parent\":{},\"varieties\":[]}},\"quantity\":{\"units\":\"GRAMMES\",\"number\":900},\"notes\":{\"en\":\"Neck fillets, cut into large chunks\"}},{\"item\":{\"name\":\"Smoked Streaky Bacon\",\"canonicalItem\":{\"canonicalName\":\"Smoked Streaky Bacon\",\"parent\":{},\"varieties\":[]}},\"quantity\":{\"units\":\"GRAMMES\",\"number\":200},\"notes\":{\"en\":\"Preferably in one piece, skinned and cut into pieces\"}}]}],\"tagsMap\":{\"SERVES_COUNT\":\"4\"}}"));
+		assertThat( recipeJson, is("{\"stages\":[{\"ingredients\":[{\"item\":{\"name\":\"Lamb Neck\",\"canonicalItem\":{\"canonicalName\":\"Lamb Neck\",\"parent\":{\"canonicalName\":\"Lamb\",\"varieties\":[]},\"varieties\":[]}},\"quantity\":{\"units\":\"GRAMMES\",\"number\":900},\"notes\":{\"en\":\"Neck fillets, cut into large chunks\"}},{\"item\":{\"name\":\"Smoked Streaky Bacon\",\"canonicalItem\":{\"canonicalName\":\"Smoked Streaky Bacon\",\"parent\":{\"canonicalName\":\"Bacon\",\"varieties\":[]},\"varieties\":[]}},\"quantity\":{\"units\":\"GRAMMES\",\"number\":200},\"notes\":{\"en\":\"Preferably in one piece, skinned and cut into pieces\"}}]}],\"tagsMap\":{\"SERVES_COUNT\":\"4\"}}"));
 
 //		final Recipe retrievedRecipe = GSON.fromJson( recipeJson, Recipe.class);
 //		assertThat( r, is(retrievedRecipe));
