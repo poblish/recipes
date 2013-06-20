1d3ff34aeaa908320d1f988f0bcf166b46089134
Andrew Regan <aregan@gmail.com>
2013-06-21 00:51:22 +0100

    Add SPICE and HERB

--- src/main/java/uk/co/recipes/api/CommonTags.java
+++ src/main/java/uk/co/recipes/api/CommonTags.java
@@ -11,5 +11,11 @@ package uk.co.recipes.api;
  */
 public enum CommonTags implements ITag {
 
-	SERVES_COUNT, VEGETARIAN, VEGAN, NUT_FREE, MEAT, VEGETABLE, SPICE, OIL
+	SERVES_COUNT, VEGETARIAN, VEGAN, NUT_FREE, MEAT, VEGETABLE, OIL,
+
+	// Spices are obtained from roots, flowers, fruits, seeds or bark
+	SPICE, // Cinnamon, Ginger, Cloves, Saffron, Nutmeg, Vanilla, Cumin
+
+	// Herbs are obtained from the leaves of herbaceous (non-woody) plants.
+	HERB // Thyme, Sage, Oregano, Parsley, Marjoram, Basil, Chives, Rosemary, Mint
 }
