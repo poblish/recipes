d546214aa4f67f8b24a83c070cbaae565d8ed5ac
Andrew Regan <aregan@gmail.com>
2013-06-23 02:22:59 +0100

    Try loading in item details via YAML

---
+++ src/main/java/uk/co/recipes/persistence/ItemsLoader.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package uk.co.recipes.persistence;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
+
+import uk.co.recipes.CanonicalItem;
+import uk.co.recipes.api.CommonTags;
+import uk.co.recipes.api.ICanonicalItem;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Supplier;
+import com.google.common.io.Files;
+
+/**
+ * TODO
+ *
+ * @author andrewregan
+ *
+ */
+public class ItemsLoader {
+
+	private final static Optional<ICanonicalItem> MISSING = Optional.absent();
+
+	public static void load() throws IOException {
+		for ( Object each : new Yaml().loadAll( Files.toString( new File("src/test/resources/inputs.yaml"), Charset.forName("utf-8")))) {
+
+			@SuppressWarnings("unchecked")
+			final Map<String,Object> map = (Map<String,Object>) each;
+
+			final String name = (String) map.get("canonicalName");
+			final String parentName = (String) map.get("parent");
+			final Optional<ICanonicalItem> parentCI = ( parentName != null) ? CanonicalItemFactory.get(parentName) : MISSING;
+
+			CanonicalItemFactory.getOrCreate( name, new Supplier<ICanonicalItem>() {
+
+				@Override
+				public ICanonicalItem get() {
+					final ICanonicalItem newItem = new CanonicalItem( name, parentCI);
+
+					final Object tagsObj = map.get("tags");
+					if ( tagsObj instanceof String) {
+						newItem.addTag( CommonTags.valueOf((String) tagsObj) );
+					}
+					else if ( tagsObj instanceof Map) {
+						@SuppressWarnings("unchecked")
+						Map<String,Object> m = (Map<String,Object>) tagsObj;
+						for ( String each : m.keySet()) {
+							newItem.addTag( CommonTags.valueOf(each) );
+						}
+					}
+
+					return newItem;
+				}});
+		}
+	}
+}
