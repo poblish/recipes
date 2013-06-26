7a1ac57370e8cb2e24e8344efa1b28aa17e53de6
Andrew Regan <aregan@gmail.com>
2013-06-27 00:36:24 +0100

    New NonNumericQuantityParser

--- src/main/java/uk/co/recipes/api/NonNumericQuantities.java
+++ src/main/java/uk/co/recipes/api/NonNumericQuantities.java
@@ -11,5 +11,5 @@ package uk.co.recipes.api;
  */
 public enum NonNumericQuantities {
 
-	SMALL, LARGE, GENEROUS, ANY_AMOUNT
+	SMALL, LARGE, GENEROUS, ANY_AMOUNT, THUMB_SIZE
 }
---
+++ src/main/java/uk/co/recipes/parse/NonNumericQuantityParser.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package uk.co.recipes.parse;
+
+import uk.co.recipes.api.NonNumericQuantities;
+
+/**
+ * TODO
+ *
+ * @author andrewregan
+ *
+ */
+public class NonNumericQuantityParser {
+
+	public static NonNumericQuantities parse( final String inStr) {
+		if (inStr.equals("THUMB-SIZE")) {
+			return NonNumericQuantities.THUMB_SIZE;
+		}
+
+		return NonNumericQuantities.valueOf(inStr);
+	}
+}
