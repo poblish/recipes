6b1be35d5ff3b89725f4920da74a95f6629d4b1a
Andrew Regan <aregan@gmail.com>
2013-07-02 23:50:17 +0100

    Bring back Elasticsearch Java API and fix Aliases search once and for all

--- src/main/java/uk/co/recipes/persistence/CanonicalItemFactory.java
+++ src/main/java/uk/co/recipes/persistence/CanonicalItemFactory.java
@@ -4,6 +4,8 @@
 package uk.co.recipes.persistence;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static org.elasticsearch.index.query.QueryBuilders.termQuery;
+import static org.elasticsearch.search.sort.SortOrder.DESC;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -11,7 +13,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
-import java.net.URLEncoder;
 import java.util.Collection;
 
 import org.apache.http.HttpResponse;
@@ -21,9 +22,13 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
-import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.search.SearchHit;
 
 import uk.co.recipes.CanonicalItem;
 import uk.co.recipes.api.ICanonicalItem;
@@ -40,10 +45,21 @@ import com.google.common.base.Throwables;
  */
 public class CanonicalItemFactory {
 
+	private static Client ES_CLIENT;
+
 	private final static HttpClient CLIENT = new DefaultHttpClient();
 
 	private final static String	IDX_URL = "http://localhost:9200/recipe/items";
 
+	// FIXME - rubbish
+	public static void startES() {
+		ES_CLIENT = new TransportClient().addTransportAddress(new InetSocketTransportAddress("localhost", 9300));
+	}
+
+	// FIXME - rubbish
+	public static void stopES() {
+		ES_CLIENT.close();
+	}
 
 	public static ICanonicalItem put( final ICanonicalItem inItem, String inId) throws IOException {
 		final HttpPost req = new HttpPost( IDX_URL + "/" + inId);
@@ -94,17 +110,18 @@ public class CanonicalItemFactory {
 
 			if (inMatchAliases) {
 				try {
-					final JsonNode foundAliasNode = JacksonFactory.getMapper().readTree( new URL(IDX_URL + "/_search?q=aliases:" + URLEncoder.encode( inCanonicalName, "UTF-8") +"&sort=_score") ).path("hits").path("hits").get(0);
+					final SearchResponse resp = ES_CLIENT.prepareSearch("recipe").setTypes("items").setQuery( termQuery( "aliases", inCanonicalName.toLowerCase()) ).addSort( "_score", DESC).execute().actionGet();
+					final SearchHit[] hits = resp.getHits().hits();
 
-					if ( foundAliasNode != null) {
-						final ICanonicalItem mappedAlias = JacksonFactory.getMapper().readValue( foundAliasNode.path("_source"), CanonicalItem.class);
+					if ( hits.length > 0) {
+						final ICanonicalItem mappedAlias = JacksonFactory.getMapper().readValue( hits[0].getSourceAsString(), CanonicalItem.class);
 						if ( mappedAlias != null) {
 							System.out.println("Successfully mapped Alias '" + inCanonicalName + "' => " + mappedAlias);
 							return mappedAlias;
 						}
 					}
 				}
-				catch (IOException e) { /* Not found! */ }
+				catch (IOException e) { /* e.printStackTrace(); */ }
 			}
 
 			System.out.println("Creating '" + inCanonicalName + "' ...");
