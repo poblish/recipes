86118a40fd6220abe996c5ac6e8c584d59437c9b
Andrew Regan <aregan@gmail.com>
2013-07-28 16:39:54 +0100

    Add ability to get unique sequence numbers from Elasticsearch

--- src/main/java/uk/co/recipes/persistence/EsUtils.java
+++ src/main/java/uk/co/recipes/persistence/EsUtils.java
@@ -6,6 +6,7 @@ package uk.co.recipes.persistence;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Collection;
+import java.util.concurrent.ExecutionException;
 
 import javax.inject.Inject;
 
@@ -13,7 +14,9 @@ import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.elasticsearch.client.Client;
 
+import com.google.common.base.Throwables;
 import com.google.common.collect.Lists;
 
 /**
@@ -27,6 +30,9 @@ public class EsUtils {
 	@Inject
 	ObjectMapper mapper;
 
+	@Inject
+	Client esClient;
+
 	// FIXME - pretty lame!
 	public <T> Collection<T> listAll( final String inBaseUrl, Class<T> inClass) throws JsonParseException, JsonMappingException, IOException {
 		final JsonNode allNodes = mapper.readTree( new URL( inBaseUrl + "/_search?q=*&size=9999") ).path("hits").path("hits");
@@ -38,5 +44,22 @@ public class EsUtils {
 		}
 
 		return allItems;
+	}
+
+	/**
+	 * Inspired by http://blogs.perl.org/users/clinton_gormley/2011/10/elasticsearchsequence---a-blazing-fast-ticket-server.html
+	 * @param inIdForEntityType the 'type' of sequence
+	 * @return a unique seqno for this 'inIdForEntityType' value
+	 */
+	public long getSeqnoForType( final String inIdForEntityType) {
+		try {
+			return esClient.prepareIndex( "sequence", "sequence").setId(inIdForEntityType).setSource("{}").execute().get().getVersion();
+		}
+		catch (InterruptedException e) {
+			throw Throwables.propagate(e);
+		}
+		catch (ExecutionException e) {
+			throw Throwables.propagate(e);
+		}
 	}
 }
