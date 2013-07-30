b8f8c480ca1691e5b2281ee4be799fe7673dbd0b
Andrew Regan <aregan@gmail.com>
2013-07-31 00:25:22 +0100

    Intercept new Items and store Myrrix item tags

--- src/main/java/uk/co/recipes/events/impl/MyrrixUpdater.java
+++ src/main/java/uk/co/recipes/events/impl/MyrrixUpdater.java
@@ -6,7 +6,9 @@ package uk.co.recipes.events.impl;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.io.Serializable;
 import java.io.StringReader;
+import java.util.Map.Entry;
 
 import javax.inject.Inject;
 
@@ -16,6 +18,7 @@ import org.apache.mahout.cf.taste.common.TasteException;
 import org.elasticsearch.common.base.Throwables;
 
 import uk.co.recipes.Recipe;
+import uk.co.recipes.api.ITag;
 import uk.co.recipes.events.api.IEventListener;
 import uk.co.recipes.events.api.IEventService;
 
@@ -42,6 +45,34 @@ public class MyrrixUpdater implements IEventListener {
     public void onAddItem( final ItemEvent evt) {
         System.out.println("# Add: " + evt);
         // throw new RuntimeException("unimpl");
+
+        final long itemId = evt.getItem().getId();
+        boolean changesMade = false;
+
+        try {
+        	for ( final Entry<ITag,Serializable> eachTag : evt.getItem().getTags().entrySet()) {
+        		if ( eachTag.getValue() instanceof Boolean) {
+        			if ((Boolean) eachTag.getValue()) {
+        				// Don't bother setting if == FALSE
+        	        	recommender.setItemTag( eachTag.getKey().toString(), itemId, 1.0f);
+        	        	changesMade = true;
+        			}
+        		}
+        		else {
+        			final float val = Float.valueOf((String) eachTag.getValue());
+    	        	recommender.setItemTag( eachTag.getKey().toString(), itemId, val);
+    	        	changesMade = true;
+        		}
+        	}
+	    }
+		catch (TasteException e) {
+			Throwables.propagate(e);
+		}
+
+        if (changesMade) {
+        	recommender.refresh();
+//            System.out.println("# Changes saved!");
+        }
     }
 
     @Override
