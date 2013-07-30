44d244d68fe4e3656d43e959b60f39a638952b24
Andrew Regan <aregan@gmail.com>
2013-07-30 22:56:52 +0100

    Comment-out unused

--- src/test/java/uk/co/recipes/taste/MyrrixTest.java
+++ src/test/java/uk/co/recipes/taste/MyrrixTest.java
@@ -14,7 +14,6 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import uk.co.recipes.DaggerModule;
-import uk.co.recipes.service.impl.MyrrixRecommendationService;
 import uk.co.recipes.service.taste.impl.MyrrixTasteRecommendationService;
 import uk.co.recipes.service.taste.impl.MyrrixTasteSimilarityService;
 import dagger.ObjectGraph;
@@ -31,7 +30,7 @@ public class MyrrixTest {
 	private final static ObjectGraph GRAPH = ObjectGraph.create( new DaggerModule() );
 
 	private MyrrixTasteRecommendationService api = GRAPH.get( MyrrixTasteRecommendationService.class );
-	private MyrrixRecommendationService fullApi = GRAPH.get( MyrrixRecommendationService.class );
+	// private MyrrixRecommendationService fullApi = GRAPH.get( MyrrixRecommendationService.class );
 
 	private MyrrixTasteSimilarityService explorerApi = GRAPH.get( MyrrixTasteSimilarityService.class );
 
