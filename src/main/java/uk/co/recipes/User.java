f4d47d930b30f0ee1821a3ba7fb188c421b98490
Andrew Regan <aregan@gmail.com>
2013-07-31 00:05:21 +0100

    Add equals/hashCode/toString

--- src/main/java/uk/co/recipes/User.java
+++ src/main/java/uk/co/recipes/User.java
@@ -7,6 +7,8 @@ import org.elasticsearch.common.Preconditions;
 
 import uk.co.recipes.api.IUser;
 
+import com.google.common.base.Objects;
+
 /**
  * TODO
  *
@@ -34,5 +36,31 @@ public class User implements IUser {
 		Preconditions.checkArgument( inId >= 0, "New Id must be >= 0 [" + inId +"]");
 		Preconditions.checkState( id == UNSET_ID, "Cannot change Item Id");
 		id = inId;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(id);
+	}
+
+	@Override
+	public boolean equals( Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof User)) {
+			return false;
+		}
+		final User other = (User) obj;
+		return ( id == other.id);
+	}
+
+	public String toString() {
+		return Objects.toStringHelper(this).omitNullValues()
+						.add( "id", ( id == UNSET_ID) ? "NEW" : Long.valueOf(id))
+						.toString();
 	}
 }
