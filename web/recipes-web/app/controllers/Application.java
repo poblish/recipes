726f771fe943ad69275f18bc5d166f18a7d90434
Andrew Regan <aregan@gmail.com>
2013-08-05 01:10:49 +0100

    First working Play page that calls Recipe Explorer service!!

--- web/recipes-web/app/controllers/Application.java
+++ web/recipes-web/app/controllers/Application.java
@@ -1,14 +1,37 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.io.IOException;
+import java.util.List;
 
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import uk.co.recipes.DaggerModule;
+import uk.co.recipes.api.ICanonicalItem;
+import uk.co.recipes.persistence.EsItemFactory;
+import uk.co.recipes.service.api.IExplorerAPI;
+import uk.co.recipes.service.api.IItemPersistence;
+import uk.co.recipes.service.impl.MyrrixExplorerService;
+import views.html.index;
+import dagger.ObjectGraph;
 
 public class Application extends Controller {
-  
-    public static Result index() {
+
+	private final static ObjectGraph GRAPH = ObjectGraph.create( new DaggerModule() );
+	private final static IItemPersistence itemFactory = GRAPH.get( EsItemFactory.class );
+    private final static IExplorerAPI explorerApi = GRAPH.get( MyrrixExplorerService.class );
+
+	public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
-  
+
+    public static Result test() throws IOException {
+        final ICanonicalItem item = itemFactory.get("ginger").get();
+        final List<ICanonicalItem> similarities = explorerApi.similarIngredients( item, 10);
+
+        return ok(views.html.test.render("" + similarities));
+    }
+
+    public static Result search() {
+        return ok(views.html.test.render("Hello, Wold!"));
+    }
 }
