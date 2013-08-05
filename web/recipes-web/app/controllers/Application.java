0b7eeeac9e7db2a85f8bdfe6a50e18f74c1541cf
Andrew Regan <aregan@gmail.com>
2013-08-05 13:05:07 +0100

    Extend to show Item recommendations for User

--- web/recipes-web/app/controllers/Application.java
+++ web/recipes-web/app/controllers/Application.java
@@ -1,8 +1,14 @@
 package controllers;
 
+import uk.co.recipes.persistence.EsUserFactory;
+import uk.co.recipes.service.api.IUserPersistence;
+import com.google.common.base.Supplier;
+import uk.co.recipes.User;
+import uk.co.recipes.api.IUser;
+import uk.co.recipes.service.impl.MyrrixRecommendationService;
+import uk.co.recipes.service.api.IRecommendationsAPI;
 import java.io.IOException;
 import java.util.List;
-
 import play.mvc.Controller;
 import play.mvc.Result;
 import uk.co.recipes.DaggerModule;
@@ -18,7 +24,9 @@ public class Application extends Controller {
 
 	private final static ObjectGraph GRAPH = ObjectGraph.create( new DaggerModule() );
 	private final static IItemPersistence ITEMS = GRAPH.get( EsItemFactory.class );
+    private final static IUserPersistence USERS = GRAPH.get( EsUserFactory.class );
     private final static IExplorerAPI EXPLORER_API = GRAPH.get( MyrrixExplorerService.class );
+    private final static IRecommendationsAPI RECS_API = GRAPH.get( MyrrixRecommendationService.class );
 
 	public static Result index() {
         return ok(index.render("Your new application is ready."));
@@ -31,6 +39,15 @@ public class Application extends Controller {
         final ICanonicalItem item = ITEMS.get(theInput).get();
         final List<ICanonicalItem> similarities = EXPLORER_API.similarIngredients( item, 10);
 
-        return ok(views.html.test.render( theInput, similarities, gotInput ? "Search Results" : "Test"));
+        final IUser user1 = USERS.getOrCreate( "Andrew Regan", new Supplier<IUser>() {
+
+            @Override
+            public IUser get() {
+                return new User();
+            }
+        } );
+        final List<ICanonicalItem> recommendations = RECS_API.recommendIngredients( user1, 10);
+
+        return ok(views.html.test.render( theInput, similarities, user1, recommendations, gotInput ? "Search Results" : "Test"));
     }
 }
