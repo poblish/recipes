9296064478c5cb4c443c3d7586c15d86b11bb7f2
Andrew Regan <aregan@gmail.com>
2013-08-07 01:06:57 +0100

    Incorporate Categorisation into Recipe pages

--- web/recipes-web/app/controllers/Recipes.java
+++ web/recipes-web/app/controllers/Recipes.java
@@ -8,15 +8,19 @@ import play.mvc.Result;
 import uk.co.recipes.DaggerModule;
 import uk.co.recipes.User;
 import uk.co.recipes.api.IRecipe;
+import uk.co.recipes.api.ITag;
 import uk.co.recipes.api.IUser;
+import uk.co.recipes.cats.Categorisation;
 import uk.co.recipes.persistence.EsRecipeFactory;
 import uk.co.recipes.persistence.EsUserFactory;
 import uk.co.recipes.service.api.IExplorerAPI;
 import uk.co.recipes.service.api.IRecipePersistence;
 import uk.co.recipes.service.api.IUserPersistence;
 import uk.co.recipes.service.impl.MyrrixExplorerService;
+import uk.co.recipes.similarity.IncompatibleIngredientsException;
 
 import com.google.common.base.Supplier;
+import com.google.common.collect.Multiset;
 
 import dagger.ObjectGraph;
 
@@ -34,16 +38,21 @@ public class Recipes extends Controller {
     private final static IUserPersistence USERS = GRAPH.get( EsUserFactory.class );
     private final static IExplorerAPI EXPLORER_API = GRAPH.get( MyrrixExplorerService.class );
 
-    public static Result display( final String name) throws IOException {
+    public static Result display( final String name) throws IOException, IncompatibleIngredientsException {
         final IRecipe recipe = RECIPES.get(name).get();
-        return ok(views.html.recipe.render( recipe, ( recipe != null) ? "Found" : "Not Found"));
+        final Multiset<ITag> categorisation = Categorisation.forIngredients( recipe.getIngredients() );
+
+        return ok(views.html.recipe.render( recipe, categorisation, ( recipe != null) ? "Found" : "Not Found"));
     }
 
-    public static Result test() throws IOException {
+    public static Result test() throws IOException, IncompatibleIngredientsException {
         final String[] theInputs = request().queryString().get("input");
         final boolean gotInput = ( theInputs != null && theInputs.length > 0 && !theInputs[0].isEmpty());
         final String theInput = gotInput ? theInputs[0] : "inputs3.txt";
+
         final IRecipe recipe = RECIPES.get(theInput).get();
+        final Multiset<ITag> categorisation = Categorisation.forIngredients( recipe.getIngredients() );
+
         final List<IRecipe> similarities = EXPLORER_API.similarRecipes( recipe, 10);
 
         final IUser user1 = USERS.getOrCreate( "Andrew Regan", new Supplier<IUser>() {
@@ -53,6 +62,6 @@ public class Recipes extends Controller {
                 return new User( "aregan", "Andrew Regan");
             }
         } );
-        return ok(views.html.recipes.render( theInput, similarities, user1, gotInput ? "Search Results" : "Test"));
+        return ok(views.html.recipes.render( theInput, categorisation, similarities, user1, gotInput ? "Search Results" : "Test"));
     }
 }
