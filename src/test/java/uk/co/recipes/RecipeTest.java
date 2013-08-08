a40ba6ccc497f52ce20430a3a4182d824cf126a9
Andrew Regan <aregan@gmail.com>
2013-08-08 11:39:41 +0100

    Test Recipe cloneability

--- src/test/java/uk/co/recipes/RecipeTest.java
+++ src/test/java/uk/co/recipes/RecipeTest.java
@@ -24,7 +24,7 @@ import uk.co.recipes.tags.CommonTags;
 public class RecipeTest {
 
 	@Test
-	public void testEqualsHash() {
+	public void testEqualsHashAndClone() {
 		final IQuantity q1 = new Quantity( Units.GRAMMES, 100);
 
 		final ICanonicalItem ingr1 = new CanonicalItem("Lamb");
@@ -51,5 +51,11 @@ public class RecipeTest {
 		TestUtils.testEqualsHashcode(r1, r2, r3, r4, r5);
 
 		assertThat( r5.toString(), is("Recipe{title=1, id=NEW, stages=[RecipeStage{ingredients=[Ingredient{q=100 GRAMMES, item=CanonicalItem{name=Lamb}}, Ingredient{q=100 GRAMMES, item=CanonicalItem{name=Beef}}]}], tags={SERVES_COUNT=4}, locale=en_GB}"));
+
+        assertThat((Recipe) r1.clone(), is(r1));
+        assertThat((Recipe) r2.clone(), is(r2));
+        assertThat((Recipe) r3.clone(), is(r3));
+        assertThat((Recipe) r4.clone(), is(r4));
+        assertThat((Recipe) r5.clone(), is(r5));
 	}
 }
