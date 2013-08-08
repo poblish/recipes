5b32221709bdc436960c7a080f4ebb786144972a
Andrew Regan <aregan@gmail.com>
2013-08-08 10:31:32 +0100

    Fix imports

--- web/recipes-web/test/ApplicationTest.java
+++ web/recipes-web/test/ApplicationTest.java
@@ -1,22 +1,8 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.codehaus.jackson.JsonNode;
-import org.junit.*;
-
-import play.mvc.*;
-import play.test.*;
-import play.data.DynamicForm;
-import play.data.validation.ValidationError;
-import play.data.validation.Constraints.RequiredValidator;
-import play.i18n.Lang;
-import play.libs.F;
-import play.libs.F.*;
-
-import static play.test.Helpers.*;
-import static org.fest.assertions.Assertions.*;
+import org.junit.Test;
+import play.mvc.Content;
+import static org.fest.assertions.Assertions.assertThat;
+import static play.test.Helpers.contentAsString;
+import static play.test.Helpers.contentType;
 
 
 /**
