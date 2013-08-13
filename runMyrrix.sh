5b45833e0ac51e98b2a9d8c061cf06e543eada31
Andrew Regan <aregan@gmail.com>
2013-08-13 19:41:21 +0100

    Switch back to latest ES 0.90.x and Lucene 4.x, now Neo4J is no more

--- runMyrrix.sh
+++ runMyrrix.sh
@@ -1,2 +1,2 @@
 nohup java -cp "/Users/andrewregan/Development/java/recipe_explorer/myrrix-serving-1.0.1.jar:/Users/andrewregan/.m2/repository/uk/co/recipes/myrrix-rescorer/0.0.1-SNAPSHOT/myrrix-rescorer-0.0.1-SNAPSHOT.jar" net.myrrix.web.Runner --port 8080 --rescorerProviderClass=uk.co.recipes.myrrix.RecipesRescorer > /private/tmp/nohup.out 2>&1&
-nohup elasticsearch -f -D es.config=/usr/local/Cellar/elasticsearch-0.20/0.20.6/config/elasticsearch.yml     > /private/tmp/nohup.out 2>&1&
+nohup elasticsearch -f -D es.config=/usr/local/Cellar/elasticsearch/0.90.2/config/elasticsearch.yml     > /private/tmp/nohup.out 2>&1&
