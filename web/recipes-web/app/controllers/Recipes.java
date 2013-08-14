f3f259f7ebf7608ce7a4283c5a555fd663d28592
Andrew Regan <aregan@gmail.com>
2013-08-14 18:45:38 +0100

    Simplify Recipe pages

--- web/recipes-web/app/controllers/Recipes.java
+++ web/recipes-web/app/controllers/Recipes.java
@@ -8,21 +8,17 @@ import javax.inject.Inject;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-import uk.co.recipes.User;
 import uk.co.recipes.api.IRecipe;
 import uk.co.recipes.api.ITag;
-import uk.co.recipes.api.IUser;
 import uk.co.recipes.cats.Categorisation;
 import uk.co.recipes.events.impl.MyrrixUpdater;
 import uk.co.recipes.persistence.EsRecipeFactory;
 import uk.co.recipes.persistence.EsUserFactory;
 import uk.co.recipes.service.api.IExplorerAPI;
 import uk.co.recipes.service.api.IRecipePersistence;
-import uk.co.recipes.service.api.IUserPersistence;
 import uk.co.recipes.service.impl.MyrrixExplorerService;
 import uk.co.recipes.similarity.IncompatibleIngredientsException;
 
-import com.google.common.base.Supplier;
 import com.google.common.collect.Multiset;
 
 /**
@@ -34,7 +30,6 @@ import com.google.common.collect.Multiset;
  */
 public class Recipes extends Controller {
 
-    private IUserPersistence users;
     private IRecipePersistence recipes;
     private IExplorerAPI explorer;
 
@@ -42,7 +37,6 @@ public class Recipes extends Controller {
     public Recipes( final MyrrixUpdater updater, final MyrrixExplorerService explorer, final EsRecipeFactory recipes, final EsUserFactory users) {
     	updater.startListening();
         this.recipes = checkNotNull(recipes);
-        this.users = checkNotNull(users);
         this.explorer = checkNotNull(explorer);
     }
 
@@ -68,18 +62,6 @@ public class Recipes extends Controller {
     public Result test() throws IOException, IncompatibleIngredientsException {
         final String[] theInputs = request().queryString().get("input");
         final boolean gotInput = ( theInputs != null && theInputs.length > 0 && !theInputs[0].isEmpty());
-        final String theInput = gotInput ? theInputs[0] : "inputs3.txt";
-
-        final IRecipe recipe = recipes.get(theInput).get();
-        final Multiset<ITag> categorisation = Categorisation.forIngredients( recipe.getIngredients() );
-
-        final IUser user1 = users.getOrCreate( "Andrew Regan", new Supplier<IUser>() {
-
-            @Override
-            public IUser get() {
-                return new User( "aregan", "Andrew Regan");
-            }
-        } );
-        return ok(views.html.recipes.render( theInput, recipe, categorisation, explorer.similarRecipes( recipe, 10), user1, gotInput ? "Search Results" : "Test"));
+        return display( gotInput ? theInputs[0] : "inputs3.txt");
     }
 }
