97f61e8bdf1b4b21d16b8c8fcaf63e10015e950e
Andrew Regan <aregan@gmail.com>
2013-08-16 00:12:07 +0100

    More ratings support

--- web/recipes-web/app/controllers/Items.java
+++ web/recipes-web/app/controllers/Items.java
@@ -7,12 +7,20 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import com.google.common.base.Supplier;
+
 import play.mvc.Controller;
 import play.mvc.Result;
+import uk.co.recipes.User;
 import uk.co.recipes.api.ICanonicalItem;
+import uk.co.recipes.api.IUser;
 import uk.co.recipes.persistence.EsItemFactory;
+import uk.co.recipes.persistence.EsUserFactory;
+import uk.co.recipes.ratings.ItemRating;
+import uk.co.recipes.ratings.UserRatings;
 import uk.co.recipes.service.api.IExplorerAPI;
 import uk.co.recipes.service.api.IItemPersistence;
+import uk.co.recipes.service.api.IUserPersistence;
 import uk.co.recipes.service.impl.MyrrixExplorerService;
 
 /**
@@ -25,12 +33,16 @@ import uk.co.recipes.service.impl.MyrrixExplorerService;
 public class Items extends Controller {
 
     private IItemPersistence items;
+    private IUserPersistence users;
     private IExplorerAPI explorerService;
+    private UserRatings ratings;
 
     @Inject
-    public Items( final EsItemFactory items, final MyrrixExplorerService inExplorerService) {
+    public Items( final EsItemFactory items, final MyrrixExplorerService inExplorerService, final EsUserFactory users, final UserRatings inRatings) {
         this.items = checkNotNull(items);
         this.explorerService = checkNotNull(inExplorerService);
+        this.users = checkNotNull(users);
+        this.ratings = checkNotNull(inRatings);
     }
 
     public Result display( final String name) throws IOException {
@@ -40,6 +52,18 @@ public class Items extends Controller {
     }
 
     public Result rate( final String name, final int inScore) throws IOException {
-        return display(name);
+        final ICanonicalItem item = items.get(name).get();
+
+		final IUser user1 = users.getOrCreate( "Andrew Regan", new Supplier<IUser>() {  // FIXME Need actual user!
+
+			@Override
+			public IUser get() {
+				return new User( "aregan", "Andrew Regan");
+			}
+		} );
+
+		ratings.addRating( user1, new ItemRating( item, inScore) );
+  
+        return redirect("/items/" + item.getCanonicalName());  // FIXME - horrible way to reload!
     }
 }
--- web/recipes-web/app/controllers/Recipes.java
+++ web/recipes-web/app/controllers/Recipes.java
@@ -8,19 +8,26 @@ import javax.inject.Inject;
 
 import play.mvc.Controller;
 import play.mvc.Result;
+import uk.co.recipes.User;
 import uk.co.recipes.api.ICanonicalItem;
 import uk.co.recipes.api.IRecipe;
 import uk.co.recipes.api.ITag;
+import uk.co.recipes.api.IUser;
 import uk.co.recipes.cats.Categorisation;
 import uk.co.recipes.events.impl.MyrrixUpdater;
 import uk.co.recipes.persistence.EsItemFactory;
 import uk.co.recipes.persistence.EsRecipeFactory;
+import uk.co.recipes.persistence.EsUserFactory;
+import uk.co.recipes.ratings.RecipeRating;
+import uk.co.recipes.ratings.UserRatings;
 import uk.co.recipes.service.api.IExplorerAPI;
 import uk.co.recipes.service.api.IItemPersistence;
 import uk.co.recipes.service.api.IRecipePersistence;
+import uk.co.recipes.service.api.IUserPersistence;
 import uk.co.recipes.service.impl.MyrrixExplorerService;
 import uk.co.recipes.similarity.IncompatibleIngredientsException;
 
+import com.google.common.base.Supplier;
 import com.google.common.collect.Multiset;
 
 /**
@@ -34,14 +41,18 @@ public class Recipes extends Controller {
 
     private IItemPersistence items;
     private IRecipePersistence recipes;
+    private IUserPersistence users;
     private IExplorerAPI explorer;
+    private UserRatings ratings;
 
     @Inject
-    public Recipes( final MyrrixUpdater updater, final MyrrixExplorerService explorer, final EsItemFactory items, final EsRecipeFactory recipes) {
+    public Recipes( final MyrrixUpdater updater, final MyrrixExplorerService explorer, final EsItemFactory items, final EsRecipeFactory recipes, final EsUserFactory users, final UserRatings inRatings) {
     	updater.startListening();
         this.items = checkNotNull(items);
         this.recipes = checkNotNull(recipes);
+        this.users = checkNotNull(users);
         this.explorer = checkNotNull(explorer);
+        this.ratings = checkNotNull(inRatings);
     }
 
     public Result fork( final String name) throws IOException, IncompatibleIngredientsException, InterruptedException {
@@ -64,7 +75,19 @@ public class Recipes extends Controller {
     }
 
     public Result rate( final String name, final int inScore) throws IOException, IncompatibleIngredientsException {
-        return display(name);
+        final IRecipe recipe = recipes.get(name).get();
+
+		final IUser user1 = users.getOrCreate( "Andrew Regan", new Supplier<IUser>() {  // FIXME Need actual user!
+
+			@Override
+			public IUser get() {
+				return new User( "aregan", "Andrew Regan");
+			}
+		} );
+
+		ratings.addRating( user1, new RecipeRating( recipe, inScore) );
+  
+        return redirect("/recipes/" + recipe.getTitle());  // FIXME - horrible way to reload!
     }
 
     public Result removeIngredient( final String name, final String ingredient) throws IOException, IncompatibleIngredientsException, InterruptedException {
@@ -76,7 +99,7 @@ public class Recipes extends Controller {
         recipes.removeItems( recipe, theItemToRemove);
         recipes.waitUntilRefreshed();
 
-        return redirect("/recipes/" + recipe.getTitle());  // FIXME - horrible title stuff
+        return redirect("/recipes/" + recipe.getTitle());  // FIXME - horrible way to reload!
     }
 
     public Result test() throws IOException, IncompatibleIngredientsException {
