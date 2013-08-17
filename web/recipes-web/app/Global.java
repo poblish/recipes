48441693123fdfe14f580e2ca678a42d78b14534
Andrew Regan <aregan@gmail.com>
2013-08-17 01:23:02 +0100

    Fix URLs

--- web/recipes-web/app/Global.java
+++ web/recipes-web/app/Global.java
@@ -9,6 +9,7 @@ import com.feth.play.module.pa.PlayAuthenticate.Resolver;
 import com.feth.play.module.pa.exceptions.AccessDeniedException;
 import com.feth.play.module.pa.exceptions.AuthException;
 
+import controllers.routes;
 import dagger.Module;
 import dagger.ObjectGraph;
 
@@ -30,34 +31,31 @@ public class Global extends GlobalSettings {
 
             @Override
             public Call login() {
-                // Your login page
-                return null; // routes.Application.index();
+                return /* FIXME */ routes.Application.index();
             }
 
             @Override
             public Call afterAuth() {
-                // The user will be redirected to this page after authentication
-                // if no original URL was saved
-                return null; // routes.Application.index();
+                // The user will be redirected to this page after authentication if no original URL was saved
+                return /* FIXME */ routes.Application.index();
             }
 
             @Override
             public Call afterLogout() {
-                return null; // routes.Application.index();
+                return /* FIXME */ routes.Application.index();
             }
 
             @Override
             public Call auth(final String provider) {
                 // You can provide your own authentication implementation,
                 // however the default should be sufficient for most cases
-                return com.feth.play.module.pa.controllers.routes.Authenticate
-                        .authenticate(provider);
+                return com.feth.play.module.pa.controllers.routes.Authenticate.authenticate(provider);
             }
 
             @Override
             public Call onException(final AuthException e) {
                 if (e instanceof AccessDeniedException) {
-                    return null; // routes.Application.oAuthDenied(((AccessDeniedException) e).getProviderKey());
+                    return routes.Application.oAuthDenied(((AccessDeniedException) e).getProviderKey());
                 }
 
                 // more custom problem handling here...
