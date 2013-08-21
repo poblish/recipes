043edf71194e8604f970929517c3c89b5141ad8e
Andrew Regan <aregan@gmail.com>
2013-08-21 16:59:50 +0100

    Trim input string, and prevent NPE if no input supplied

--- web/recipes-web/app/controllers/Search.java
+++ web/recipes-web/app/controllers/Search.java
@@ -1,15 +1,16 @@
 package controllers;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 import java.io.IOException;
-
+import java.util.Collections;
+import java.util.List;
 import javax.inject.Inject;
-
 import play.mvc.Controller;
 import play.mvc.Result;
+import uk.co.recipes.api.ICanonicalItem;
+import uk.co.recipes.api.IRecipe;
 import uk.co.recipes.service.api.ISearchAPI;
 import uk.co.recipes.service.impl.EsSearchService;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * 
@@ -22,6 +23,9 @@ public class Search extends Controller {
 
 	private ISearchAPI search;
 
+    private static final List<IRecipe> EMPTY_RECIPES = Collections.emptyList();
+    private static final List<ICanonicalItem> EMPTY_ITEMS = Collections.emptyList();
+
     @Inject
     public Search( final EsSearchService inSearch) {
         this.search = checkNotNull(inSearch);
@@ -32,10 +36,10 @@ public class Search extends Controller {
         final boolean gotInput = ( theInputs != null && theInputs.length > 0 && !theInputs[0].isEmpty());
 
         if (!gotInput) {
-            // Yuk, FIXME
-            return ok(views.html.search.render( null, null));
+            return ok(views.html.search.render( EMPTY_RECIPES, EMPTY_ITEMS));
         }
 
-        return ok(views.html.search.render( search.findRecipesByName( theInputs[0] ), search.findItemsByName( theInputs[0] ) ));
+        final String termToUse = theInputs[0].trim();
+        return ok(views.html.search.render( search.findRecipesByName(termToUse), search.findItemsByName(termToUse) ));
     }
 }
