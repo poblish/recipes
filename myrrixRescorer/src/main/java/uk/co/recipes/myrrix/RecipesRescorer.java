d3b173233d054b4e884adb5519cb07e81dbbd488
Andrew Regan <aregan@gmail.com>
2013-08-22 23:09:01 +0100

    Tweaks

--- myrrixRescorer/src/main/java/uk/co/recipes/myrrix/RecipesRescorer.java
+++ myrrixRescorer/src/main/java/uk/co/recipes/myrrix/RecipesRescorer.java
@@ -28,6 +28,7 @@ public class RecipesRescorer extends AbstractRescorerProvider {
 	private static final Logger LOG = LoggerFactory.getLogger( RecipesRescorer.class );
 
 	private final static long RECIPE_BASE_ID = 0x4000000000000000L;
+
 	private static final Splitter ID_SPLITTER = Splitter.on(',');
 	private static final long[] EMPTY_ARRAY = new long[0];
 
@@ -68,9 +69,6 @@ public class RecipesRescorer extends AbstractRescorerProvider {
 
 	@Override
 	public Rescorer<LongPair> getMostSimilarItemsRescorer( final MyrrixRecommender inRecommender, final String... inArgs) {
-//		LOG.info(Arrays.toString(inArgs));
-//		long toItemID = Long.parseLong( inArgs[0] );
-//		LOG.info("toItemID = " + toItemID);
 
 		final String desiredType = ( inArgs != null && inArgs.length >= 1) ? (String) inArgs[0] : "";
 		final boolean isRecipe = desiredType.equals("RECIPE");
