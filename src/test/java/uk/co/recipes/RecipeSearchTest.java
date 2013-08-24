f7047d9f2fed1700254aadc2629b176caae38f5c
Andrew Regan <aregan@gmail.com>
2013-08-24 14:09:25 +0100

    Test Explorer Recipe filters

--- src/test/java/uk/co/recipes/RecipeSearchTest.java
+++ src/test/java/uk/co/recipes/RecipeSearchTest.java
@@ -4,9 +4,11 @@
 package uk.co.recipes;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.is;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.http.client.ClientProtocolException;
 import org.elasticsearch.client.Client;
@@ -14,13 +16,16 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import uk.co.recipes.api.IRecipe;
 import uk.co.recipes.persistence.EsItemFactory;
 import uk.co.recipes.persistence.EsRecipeFactory;
 import uk.co.recipes.persistence.ItemsLoader;
-import uk.co.recipes.service.api.IItemPersistence;
+import uk.co.recipes.service.api.IExplorerFilter;
 import uk.co.recipes.service.api.IRecipePersistence;
 import uk.co.recipes.service.api.ISearchAPI;
+import uk.co.recipes.service.impl.EsExplorerFilters;
 import uk.co.recipes.service.impl.EsSearchService;
+import uk.co.recipes.tags.CommonTags;
 import uk.co.recipes.test.TestDataUtils;
 import dagger.ObjectGraph;
 
@@ -35,8 +40,9 @@ public class RecipeSearchTest {
 	private final static ObjectGraph GRAPH = ObjectGraph.create( new DaggerModule() );
 
 	private Client esClient = GRAPH.get( Client.class );
-	private IItemPersistence itemFactory = GRAPH.get( EsItemFactory.class );
 	private IRecipePersistence recipeFactory = GRAPH.get( EsRecipeFactory.class );
+
+	private EsExplorerFilters explorerFilters = GRAPH.get( EsExplorerFilters.class );
 
 	private ISearchAPI searchApi = GRAPH.get( EsSearchService.class );
 	private TestDataUtils dataUtils = GRAPH.get( TestDataUtils.class );
@@ -44,7 +50,7 @@ public class RecipeSearchTest {
 
 	@BeforeClass
 	public void cleanIndices() throws ClientProtocolException, IOException {
-		itemFactory.deleteAll();
+		GRAPH.get( EsItemFactory.class ).deleteAll();
 		recipeFactory.deleteAll();
 	}
 
@@ -66,6 +72,27 @@ public class RecipeSearchTest {
 	@Test
 	public void findGarlicRecipes() throws InterruptedException, IOException {
 		assertThat( searchApi.countRecipesByName("garlic"), is(4));
+	}
+
+	@Test
+	public void testUnusedTag() throws IOException {
+		assertThat( searchApi.findRecipesByTag( CommonTags.CITRUS ).size(), is(0));
+	}
+
+	@Test
+	public void testSearchByTagAndFilters() throws IOException {
+		final int numItems = searchApi.findItemsByTag( CommonTags.MEAT ).size();
+
+		final List<IRecipe> foundRecipes = searchApi.findRecipesByTag( CommonTags.MEAT );
+		assertThat( foundRecipes.size(), greaterThanOrEqualTo(4));  // Surely 5 ?!?
+
+		final IExplorerFilter filter = explorerFilters.includeTags( CommonTags.MEAT );
+		assertThat( filter.idsToInclude().length, is( foundRecipes.size() + numItems));
+		assertThat( filter.idsToExclude().length, is(0));
+
+		final IExplorerFilter filter2 = explorerFilters.excludeTags( CommonTags.MEAT );
+		assertThat( filter2.idsToInclude().length, is(0));
+		assertThat( filter2.idsToExclude().length, is( foundRecipes.size() + numItems));
 	}
 
 	@AfterClass
