12f7d54127fc0691f9b790e5f47fdb2994b688c0
Andrew Regan <aregan@gmail.com>
2013-08-30 00:00:44 +0100

    New tags and ingredients

--- src/main/java/uk/co/recipes/tags/CommonTags.java
+++ src/main/java/uk/co/recipes/tags/CommonTags.java
@@ -21,7 +21,7 @@ public enum CommonTags implements ITag {
 	// Herbs are obtained from the leaves of herbaceous (non-woody) plants.
 	HERB, // Thyme, Sage, Oregano, Parsley, Marjoram, Basil, Chives, Rosemary, Mint
 
-	SUGAR, SALT, FLOUR, BREAD, PASTA, SEED, NUT, PULSE, FRUIT, CITRUS, CHEESE, CHILLI, EGG,
+	SUGAR, SALT, FLOUR, BREAD, PASTA, SEED, NUT, PULSE, FRUIT, CITRUS, CHEESE, BLUE_CHEESE, CHILLI, EGG,
 
 	MEAT, OFFAL, POULTRY, GAME, FISH, SEAFOOD, STOCK, SAUSAGE,
 
@@ -30,8 +30,8 @@ public enum CommonTags implements ITag {
 	FLAVOURING, BAKING,
 
 	// Should be in FlavourTags
-	ANISEED,
+	ANISEED, SWEET, SOUR,
 
 	// Should be in NationalCuisineTags...
-	INDIAN, CHINESE, JAPANESE, THAI, VIETNAMESE, FRENCH, ITALIAN, GREEK, ENGLISH, SPANISH, HUNGARIAN, MEXICAN, GERMAN
+	INDIAN, CHINESE, JAPANESE, THAI, VIETNAMESE, FRENCH, ITALIAN, GREEK, ENGLISH, SPANISH, HUNGARIAN, MEXICAN, GERMAN, SWISS
 }
