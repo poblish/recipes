b262c593495226576864b58c4f121597fedb5bad
Andrew Regan <aregan@gmail.com>
2013-09-03 11:30:58 +0100

    Improve test

--- src/test/java/uk/co/recipes/parse/NameAdjusterTest.java
+++ src/test/java/uk/co/recipes/parse/NameAdjusterTest.java
@@ -2,7 +2,8 @@ package uk.co.recipes.parse;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.empty;
 import org.testng.annotations.Test;
 
 /**
@@ -13,8 +14,39 @@ import org.testng.annotations.Test;
  *
  */
 public class NameAdjusterTest {
-	@Test
-	public void testAdjust() {
-		assertThat( new NameAdjuster().adjust("dressed mixed leaves"), is("mixed leaves"));
-	}
+
+    @Test
+    public void testAdjust1() {
+        final NameAdjuster na = new NameAdjuster();
+        assertThat( na.adjust("dressed mixed leaves"), is("mixed leaves"));
+        assertThat( na.getExtraNotes(), hasItems("dressed"));
+    }
+
+    @Test
+    public void testAdjust2() {
+        final NameAdjuster na = new NameAdjuster();
+        assertThat( na.adjust("freshly grated nutmeg"), is("nutmeg"));
+        assertThat( na.getExtraNotes(), hasItems("freshly grated"));
+    }
+
+    @Test
+    public void testAdjust3() {
+        final NameAdjuster na = new NameAdjuster();
+        assertThat( na.adjust("whole mango"), is("mango"));
+        assertThat( na.getExtraNotes(), hasItems("whole"));
+    }
+
+    @Test
+    public void testAdjust4() {
+        final NameAdjuster na = new NameAdjuster();
+        assertThat( na.adjust("FuLL-bODIEd RED WINE"), is("RED WINE"));
+        assertThat( na.getExtraNotes(), hasItems("full-bodied"));
+    }
+
+    @Test
+    public void testAdjust5() {
+        final NameAdjuster na = new NameAdjuster();
+        assertThat( na.adjust("100g coriander seeds"), is("100g coriander seeds"));
+        assertThat( na.getExtraNotes(), empty());
+    }
 }
