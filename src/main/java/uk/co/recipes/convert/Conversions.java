86153ebd260d53325139edac68b5bb4294527406
Andrew Regan <aregan@gmail.com>
2013-09-04 01:11:36 +0100

    Add hideous JScience units-conversion code

---
+++ src/main/java/uk/co/recipes/convert/Conversions.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package uk.co.recipes.convert;
+
+
+import java.util.Locale;
+
+import javax.measure.quantity.Volume;
+import javax.measure.unit.NonSI;
+import javax.measure.unit.Unit;
+
+import uk.co.recipes.api.IQuantity;
+import uk.co.recipes.api.Units;
+
+import com.google.common.base.Optional;
+
+/**
+ * TODO
+ *
+ * @author andrewregan
+ *
+ */
+public class Conversions {
+
+	private final static Unit<Volume> TSP_UK = NonSI.LITRE.divide(1000).times(5.91939047);
+	private final static Unit<Volume> TSP_US = NonSI.LITRE.divide(1000).times(4.92892159);
+
+	public Optional<Unit<Volume>> getUnitOfVolume( final Locale inLocale, final IQuantity inQuantity) {
+		if ( inQuantity.getUnits() == Units.TSP) {
+			if ( inLocale == Locale.UK) {
+				return Optional.of( times( TSP_UK, inQuantity.getNumber() ) );
+			}
+			else {  // US is default, is it now?
+				return Optional.of( times( TSP_US, inQuantity.getNumber() ) );
+			}
+		}
+
+		return Optional.absent();
+	}
+
+	// Work around crap code
+	private Unit<Volume> times( Unit<Volume> unit, int inCount) {
+		if ( inCount == 1) {
+			return unit;
+		}
+
+		return unit.times(inCount);
+	}
+}
--- src/main/java/uk/co/recipes/service/impl/DefaultIngredientQuantityScoreBooster.java
+++ src/main/java/uk/co/recipes/service/impl/DefaultIngredientQuantityScoreBooster.java
@@ -3,11 +3,19 @@
  */
 package uk.co.recipes.service.impl;
 
+import java.util.Locale;
+
 import javax.inject.Singleton;
+import javax.measure.converter.RationalConverter;
+import javax.measure.quantity.Volume;
+import javax.measure.unit.Unit;
 
 import uk.co.recipes.api.ICanonicalItem;
 import uk.co.recipes.api.IQuantity;
+import uk.co.recipes.convert.Conversions;
 import uk.co.recipes.service.api.IIngredientQuantityScoreBooster;
+
+import com.google.common.base.Optional;
 
 
 /**
@@ -22,6 +30,19 @@ public class DefaultIngredientQuantityScoreBooster implements IIngredientQuantit
     @Override
     public float getBoostForQuantity( final ICanonicalItem inItem, final IQuantity inQuantity) {
     	if ( inItem.getBaseAmount() != null /* FIXME null check */) {
+			System.out.println( "=> " + inQuantity );
+
+			final Optional<Unit<Volume>> units = new Conversions().getUnitOfVolume( Locale.UK, inQuantity);
+    		if (units.isPresent()) {
+
+    			Optional<Unit<Volume>> baseUnits = new Conversions().getUnitOfVolume( Locale.UK, inItem.getBaseAmount());
+        		if (baseUnits.isPresent()) {
+        			System.out.println( units.get() + " / " + baseUnits.get() );
+        			Unit<?> ratio = units.get().divide( baseUnits.get() );
+        			System.out.println( "Boost = " + ratio.getDimension() );
+        			int x = 1;
+        		}
+    		}
     		// System.out.println(inItem);
     	}
         return NO_BOOST;
