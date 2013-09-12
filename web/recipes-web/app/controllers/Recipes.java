58d81dada4734a688b664dc18744dd031fc6ac25
Andrew Regan <aregan@gmail.com>
2013-09-12 01:01:47 +0100

    Support Add Ingredient in GUI; big refactor of Recipes controller

--- web/recipes-web/app/controllers/Recipes.java
+++ web/recipes-web/app/controllers/Recipes.java
@@ -7,10 +7,13 @@ import java.io.IOException;
 import javax.inject.Inject;
 
 import play.mvc.Result;
+import uk.co.recipes.Ingredient;
+import uk.co.recipes.Quantity;
 import uk.co.recipes.api.ICanonicalItem;
 import uk.co.recipes.api.IRecipe;
 import uk.co.recipes.api.ITag;
 import uk.co.recipes.api.IUser;
+import uk.co.recipes.api.Units;
 import uk.co.recipes.cats.Categorisation;
 import uk.co.recipes.events.impl.MyrrixUpdater;
 import uk.co.recipes.persistence.EsItemFactory;
@@ -63,7 +66,7 @@ public class Recipes extends AbstractExplorableController {
         }
         while (!recipes.getById( fork.getId() ).isPresent());
 
-        return redirect("/recipes/" + fork.getTitle());  // FIXME - horrible title stuff
+        return reloadRecipe(fork);
     }
 
     public Result display( final String name) throws IOException {
@@ -78,44 +81,72 @@ public class Recipes extends AbstractExplorableController {
         return ok(views.html.recipe.render( recipe, categorisation, explorer.similarRecipes( recipe, getExplorerFilter(explorerFilters), 10)));
     }
 
-    public Result rate( final String name, final int inScore) throws IOException {
-		final IUser user1 = getLocalUser();
-		if ( user1 == null) {
-		    return unauthorized("Not logged-in");
-		}
+    public Result rate( final String name, final int inScore) throws IOException, InterruptedException {
+    	return handleLoggedInRecipeBasedAction( name, new RecipeAction() {
 
-        final Optional<IRecipe> optRecipe = recipes.get(name);
-        if (!optRecipe.isPresent()) {
-            return notFound("'" + name + "' not found!");
-        }
-
-        final IRecipe recipe = optRecipe.get();
-		ratings.addRating( user1, new RecipeRating( recipe, inScore) );
-  
-        return redirect("/recipes/" + recipe.getTitle());  // FIXME - horrible way to reload!
+			@Override
+			public Result doAction( final IUser loggedInUser, final IRecipe recipe) throws IOException, InterruptedException {
+				ratings.addRating( loggedInUser, new RecipeRating( recipe, inScore) );
+				  
+		        return reloadRecipe(recipe);
+			}
+		} );
     }
 
     public Result removeIngredient( final String name, final String ingredient) throws IOException, InterruptedException {
+    	return handleLoggedInRecipeBasedAction( name, new RecipeAction() {
+
+			@Override
+			public Result doAction( final IUser loggedInUser, final IRecipe recipe) throws IOException, InterruptedException {
+		        // FIXME Check recipe.getForkDetails(), also creator vs. current user for permissions!
+
+		        final ICanonicalItem theItemToRemove = checkNotNull( items.get(ingredient).get(), "Could not load Item");
+
+		        recipes.removeItems( recipe, theItemToRemove);
+		        recipes.waitUntilRefreshed();
+
+		        return reloadRecipe(recipe);
+			}
+		} );
+     }
+
+    public Result addIngredient( final String name, final String ingredient) throws IOException, InterruptedException {
+    	return handleLoggedInRecipeBasedAction( name, new RecipeAction() {
+
+			@Override
+			public Result doAction( final IUser loggedInUser, final IRecipe recipe) throws IOException, InterruptedException {
+		        // FIXME Check recipe.getForkDetails(), also creator vs. current user for permissions!
+
+		        final ICanonicalItem theItemToAdd = checkNotNull( items.get(ingredient).get(), "Could not load Item");
+
+		        recipes.addIngredients( recipe, new Ingredient( theItemToAdd, /* FIXME: hardcoded: */ new Quantity( Units.GRAMMES, 100)));
+		        recipes.waitUntilRefreshed();
+
+		        return reloadRecipe(recipe);
+			}
+		} );
+     }
+
+    private Result handleLoggedInRecipeBasedAction( final String inRecipeName, final RecipeAction inAction) throws IOException, InterruptedException {
 		final IUser user1 = getLocalUser();
 		if ( user1 == null) {
 		    return unauthorized("Not logged-in");
 		}
 
-        final Optional<IRecipe> optRecipe = recipes.get(name);
+        final Optional<IRecipe> optRecipe = recipes.get(inRecipeName);
         if (!optRecipe.isPresent()) {
-            return notFound("'" + name + "' not found!");
+            return notFound("'" + inRecipeName + "' not found!");
         }
 
-        final IRecipe recipe = optRecipe.get();
+        return inAction.doAction( user1, optRecipe.get() );
+    }
 
-        // FIXME Check recipe.getForkDetails(), also creator vs. current user for permissions!
+    private Result reloadRecipe( final IRecipe inRecipe) {
+        return redirect("/recipes/" + inRecipe.getTitle());  // FIXME - horrible way to reload!
+    }
 
-        final ICanonicalItem theItemToRemove = checkNotNull( items.get(ingredient).get(), "Could not load Item");
-
-        recipes.removeItems( recipe, theItemToRemove);
-        recipes.waitUntilRefreshed();
-
-        return redirect("/recipes/" + recipe.getTitle());  // FIXME - horrible way to reload!
+    private interface RecipeAction {
+		Result doAction( IUser loggedInUser, IRecipe recipe) throws IOException, InterruptedException;
     }
 
     public Result test() throws IOException {
--- web/recipes-web/public/javascripts/main.js
+++ web/recipes-web/public/javascripts/main.js
@@ -2,3 +2,8 @@ function getForkName(inRecipeTitle) {
 	bootbox.prompt('Please name your new recipe', function(res) { if ( res != null && res != '') { document.location.href = '/recipes/' + inRecipeTitle + '/fork?newName=' + res; } });
 	return false;
 }
+
+function addIngredient(inRecipeTitle, inIngredientSelect) {
+	document.location.href = '/recipes/' + inRecipeTitle + '/' + inIngredientSelect.select2('data').displayName + '/add';
+	return false;
+}
