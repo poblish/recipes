e814b0b4896880d12375bd5ebda36807da7c0cf7
Andrew Regan <aregan@gmail.com>
2013-09-23 00:56:18 +0100

    Add -Dmodel.features=20 -Dmodel.als.lambda=0.25 to Myrrix script

--- runMyrrix.sh
+++ runMyrrix.sh
@@ -1,5 +1,5 @@
 pkill -f net.myrrix.web.Runner
 pkill -f org.elasticsearch.bootstrap.ElasticSearch
 
-nohup java -cp "/Users/andrewregan/Development/java/recipe_explorer/myrrix-serving-1.0.1.jar:/Users/andrewregan/.m2/repository/uk/co/recipes/myrrix-rescorer/0.0.1-SNAPSHOT/myrrix-rescorer-0.0.1-SNAPSHOT.jar" net.myrrix.web.Runner --port 8080 --rescorerProviderClass=uk.co.recipes.myrrix.RecipesRescorer > /private/tmp/nohup.out 2>&1&
+nohup java -Dmodel.features=20 -Dmodel.als.lambda=0.25 -cp "/Users/andrewregan/Development/java/recipe_explorer/myrrix-serving-1.0.1.jar:/Users/andrewregan/.m2/repository/uk/co/recipes/myrrix-rescorer/0.0.1-SNAPSHOT/myrrix-rescorer-0.0.1-SNAPSHOT.jar" net.myrrix.web.Runner --port 8080 --rescorerProviderClass=uk.co.recipes.myrrix.RecipesRescorer > /private/tmp/nohup.out 2>&1&
 nohup elasticsearch -f -D es.config=/usr/local/Cellar/elasticsearch/0.90.2/config/elasticsearch.yml     > /private/tmp/nohup.out 2>&1&
