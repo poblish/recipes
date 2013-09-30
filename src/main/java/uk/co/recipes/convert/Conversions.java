488f16f5ede0dcf5da880cc174d306afa8773d73
Andrew Regan <aregan@gmail.com>
2013-10-01 00:28:39 +0100

    Conversions enhancement: to cover US Flour stuff

--- src/main/java/uk/co/recipes/convert/Conversions.java
+++ src/main/java/uk/co/recipes/convert/Conversions.java
@@ -4,10 +4,14 @@
 package uk.co.recipes.convert;
 
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.util.Locale;
 
+import javax.measure.quantity.Quantity;
 import javax.measure.quantity.Volume;
 import javax.measure.unit.NonSI;
+import javax.measure.unit.SI;
 import javax.measure.unit.Unit;
 
 import org.jscience.physics.amount.Amount;
@@ -16,52 +20,130 @@ import uk.co.recipes.api.IQuantity;
 import uk.co.recipes.api.IUnit;
 import uk.co.recipes.api.Units;
 
+import com.google.common.base.Objects;
 import com.google.common.base.Optional;
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
 
 /**
- * See http://www.traditionaloven.com/conversions_of_measures/flour_volume_weight.html any many others.
+ * See http://www.traditionaloven.com/conversions_of_measures/flour_volume_weight.html
+ * Also see http://allrecipes.com/howto/cup-to-gram-conversions/ - which differs significantly
  *
  * @author andrewregan
  *
  */
 public class Conversions {
 
-    // Can probably fold these into the VOLUME_UNITS_TABLE init.
-	private final static Unit<Volume> TSP_UK = NonSI.LITRE.divide(1000).times(5.91939047);
-	private final static Unit<Volume> TSP_US = NonSI.LITRE.divide(1000).times(4.92892159);
-    private final static Unit<Volume> TBSP_UK = TSP_UK.times(3);
-    private final static Unit<Volume> TBSP_US = TSP_US.times(3);
-
     private final static Locale DEFAULT_LOCALE = new Locale( "xx", "xx");
+    private final static String DEFAULT_ITEM = "_";
 
-    private final static Table<IUnit,Locale,Unit<Volume>>  VOLUME_UNITS_TABLE = HashBasedTable.create();
+    private final static Table<ItemUnit,Locale,Unit<?>>  VOLUME_UNITS_TABLE = HashBasedTable.create();
 
     static {
-        VOLUME_UNITS_TABLE.put( Units.TSP, DEFAULT_LOCALE, TSP_UK);
-        VOLUME_UNITS_TABLE.put( Units.TBSP, DEFAULT_LOCALE, TBSP_UK);
+    	final Unit<Volume> tsp_UK = NonSI.LITRE.divide(1000).times(5.91939047);
+    	final Unit<Volume> tsp_US = NonSI.LITRE.divide(1000).times(4.92892159);
+    	final Unit<Volume> tbsp_UK = tsp_UK.times(3);
+    	final Unit<Volume> tbsp_US = tsp_US.times(3);
 
-        VOLUME_UNITS_TABLE.put( Units.TSP, /* Override */ Locale.US, TSP_US);
-        VOLUME_UNITS_TABLE.put( Units.TBSP, /* Override */ Locale.US, TBSP_US);
+        VOLUME_UNITS_TABLE.put( forUnit(Units.TSP), DEFAULT_LOCALE, tsp_UK);
+        VOLUME_UNITS_TABLE.put( forUnit(Units.TBSP), DEFAULT_LOCALE, tbsp_UK);
+
+        VOLUME_UNITS_TABLE.put( new ItemUnit( Units.CUP, "Plain Flour"), /* Override */ Locale.US, SI.KILOGRAM.times(0.125));
+        VOLUME_UNITS_TABLE.put( new ItemUnit( Units.CUP, "Bread Flour"), /* Override */ Locale.US, SI.KILOGRAM.times(0.127));
+        VOLUME_UNITS_TABLE.put( new ItemUnit( Units.CUP, "Rye Flour"), /* Override */ Locale.US, SI.KILOGRAM.times(0.102));
+        VOLUME_UNITS_TABLE.put( new ItemUnit( Units.CUP, "Self-raising Flour"), /* Override */ Locale.US, SI.KILOGRAM.times(0.125));
+        VOLUME_UNITS_TABLE.put( new ItemUnit( Units.CUP, "Wholewheat Flour"), /* Override */ Locale.US, SI.KILOGRAM.times(0.120));
+        VOLUME_UNITS_TABLE.put( new ItemUnit( Units.CUP, "Rolled Oats"), /* Override */ Locale.US, SI.KILOGRAM.times(0.085));
+
+//        VOLUME_UNITS_TABLE.put( forUnit(Units.QUART), /* Override */ Locale.US, tsp_US);
+
+        VOLUME_UNITS_TABLE.put( forUnit(Units.TSP), /* Override */ Locale.US, tsp_US);
+        VOLUME_UNITS_TABLE.put( forUnit(Units.TBSP), /* Override */ Locale.US, tbsp_US);
     }
 
-	public Optional<Amount<Volume>> toJsrAmount( final Locale inLocale, final IQuantity inQuantity) {
+	public <T extends Quantity> Optional<Amount<T>> toJsrAmount( final Locale inLocale, final IQuantity inQuantity) {
+		return toJsrAmount( inLocale, forUnit( inQuantity.getUnits() ), inQuantity);
+	}
 
-	    final Unit<Volume> unitForLocale = VOLUME_UNITS_TABLE.get( inQuantity.getUnits(), inLocale);
+	public <T extends Quantity> Optional<Amount<T>> toJsrAmount( final Locale inLocale, final String inItemName, IQuantity inQuantity) {
+		return toJsrAmount( inLocale, new ItemUnit( inQuantity.getUnits(), inItemName), inQuantity);
+	}
+
+	private <T extends Quantity> Optional<Amount<T>> toJsrAmount( final Locale inLocale, final ItemUnit inItemUnit, final IQuantity inQuantity) {
+
+	    final Unit<?> unitForLocale = VOLUME_UNITS_TABLE.get( inItemUnit, inLocale);
 	    if ( unitForLocale != null) {
             return amountInUnits( inQuantity, unitForLocale);
 	    }
 
-        final Unit<Volume> defaultUnit = VOLUME_UNITS_TABLE.get( inQuantity.getUnits(), DEFAULT_LOCALE);
+        final Unit<?> defaultUnit = VOLUME_UNITS_TABLE.get( forUnit( inQuantity.getUnits() ), DEFAULT_LOCALE);
         if ( defaultUnit != null) {
             return amountInUnits( inQuantity, defaultUnit);
+        }
+
+        // Yuk, FIXME!
+
+        if ( inLocale == Locale.US) {
+            if ( inItemUnit.unit == Units.QUART) {
+            	final Optional<Amount<T>> opt = toJsrAmount( inLocale, new ItemUnit( Units.CUP, inItemUnit.canonicalName), inQuantity);
+            	if (opt.isPresent()) {
+            		return Optional.of(opt.get().times(4));  // http://en.wikipedia.org/wiki/Quart
+            	}
+            }
+            else if ( inItemUnit.unit == Units.OUNCES) {
+            	final Optional<Amount<T>> opt = toJsrAmount( inLocale, new ItemUnit( Units.CUP, inItemUnit.canonicalName), inQuantity);
+            	if (opt.isPresent()) {
+            		return Optional.of(opt.get().times(0.125));  // http://en.wikipedia.org/wiki/Fluid_ounce
+            	}
+            }
         }
 
 		return Optional.absent();
 	}
 
-	private Optional<Amount<Volume>> amountInUnits( final IQuantity inQuantity, final Unit<Volume> inUnits) {
-        return Optional.of( Amount.valueOf( inQuantity.getNumber(), inUnits) );
+	@SuppressWarnings("unchecked")
+	private <T extends Quantity> Optional<Amount<T>> amountInUnits( final IQuantity inQuantity, final Unit<?> inUnits) {
+        return Optional.of((Amount<T>) Amount.valueOf( inQuantity.getNumber(), inUnits));
+	}
+
+	private static ItemUnit forUnit( final IUnit unit) {
+		return new ItemUnit( unit, DEFAULT_ITEM);
+	}
+
+	/**
+	 * 
+	 * TODO
+	 *
+	 * @author andrewregan
+	 *
+	 */
+	private static class ItemUnit {
+		private IUnit unit;
+		private String canonicalName;
+
+		public ItemUnit( final IUnit unit, final String canonicalName) {
+			this.unit = checkNotNull(unit);
+			this.canonicalName = checkNotNull(canonicalName);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hashCode( unit, canonicalName);
+		}
+
+		@Override
+		public boolean equals( Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null) {
+				return false;
+			}
+			if (!(obj instanceof ItemUnit)) {
+				return false;
+			}
+			final ItemUnit other = (ItemUnit) obj;
+			return Objects.equal( unit, other.unit) && Objects.equal( canonicalName, other.canonicalName);
+		}
 	}
 }
--- src/test/java/uk/co/recipes/convert/ConversionsTest.java
+++ src/test/java/uk/co/recipes/convert/ConversionsTest.java
@@ -6,18 +6,24 @@ package uk.co.recipes.convert;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-import java.util.Locale;
+import static java.util.Locale.*;
 
 import javax.measure.unit.NonSI;
+import javax.measure.unit.SI;
 
+import org.jscience.physics.amount.Amount;
 import org.testng.annotations.Test;
 
 import uk.co.recipes.Quantity;
+import uk.co.recipes.api.IQuantity;
 import uk.co.recipes.api.Units;
+
+import com.google.common.base.Optional;
 
 
 /**
- * TODO
+ * See http://www.traditionaloven.com/conversions_of_measures/flour_volume_weight.html
+ * Also see http://allrecipes.com/howto/cup-to-gram-conversions/ - which differs significantly
  * 
  * @author andrewr
  *
@@ -26,23 +32,65 @@ public class ConversionsTest {
 
     private final static Conversions CVS = new Conversions();
 
+    private final static IQuantity ONE_CUP = new Quantity( Units.CUP, 1);
+    private final static IQuantity ONE_QUART = new Quantity( Units.QUART, 1);
+    private final static IQuantity ONE_OZ = new Quantity( Units.OUNCES, 1);
+    private final static IQuantity ONE_TSP = new Quantity( Units.TSP, 1);
+
     @Test
     public void testTspUK() {
-        assertThat( CVS.toJsrAmount( Locale.UK, new Quantity( Units.TSP, 1)).get().getEstimatedValue(), is(1.0));
+    	assertThat( CVS.toJsrAmount( UK, ONE_TSP).get().getEstimatedValue(), is(1.0));
     }
 
     @Test
     public void testTspUKInMilliletres() {
-        assertThat( CVS.toJsrAmount( Locale.UK, new Quantity( Units.TSP, 1)).get().to( NonSI.LITRE.divide(1000) ).getEstimatedValue(), is(5.91939047));
+        assertThat( toMl( CVS.toJsrAmount( UK, ONE_TSP) ), is(5.91939047));
     }
 
     @Test
     public void testTspUS() {
-        assertThat( CVS.toJsrAmount( Locale.US, new Quantity( Units.TSP, 1)).get().getEstimatedValue(), is(1.0));
+        assertThat( CVS.toJsrAmount( US, ONE_TSP).get().getEstimatedValue(), is(1.0));
     }
 
     @Test
     public void testMissingUnit() {
-        assertThat( CVS.toJsrAmount( Locale.UK, new Quantity( Units.SPLASHES, 1)).isPresent(), is(false));
+        assertThat( CVS.toJsrAmount( UK, new Quantity( Units.SPLASHES, 1)).isPresent(), is(false));
+    }
+
+    @Test
+    public void testFlour_USCups() {
+        assertThat( toGrammes( CVS.toJsrAmount( US, "Plain Flour", ONE_CUP) ), is(125d));
+        assertThat( toGrammes( CVS.toJsrAmount( US, "Bread Flour", ONE_CUP) ), is(127d));
+        assertThat( toGrammes( CVS.toJsrAmount( US, "Rye Flour", ONE_CUP) ), is(102d));
+        assertThat( toGrammes( CVS.toJsrAmount( US, "Self-raising Flour", ONE_CUP) ), is(125d));
+        assertThat( toGrammes( CVS.toJsrAmount( US, "Wholewheat Flour", ONE_CUP) ), is(120d));
+        assertThat( toGrammes( CVS.toJsrAmount( US, "Rolled Oats", ONE_CUP) ), is(85d));
+    }
+
+    @Test
+    public void testFlour_USQuarts() {
+        assertThat( toGrammes( CVS.toJsrAmount( US, "Plain Flour", ONE_QUART) ), is(500d));
+        assertThat( toGrammes( CVS.toJsrAmount( US, "Bread Flour", ONE_QUART) ), is(508d));
+        assertThat( toGrammes( CVS.toJsrAmount( US, "Rye Flour", ONE_QUART) ), is(408d));
+        assertThat( toGrammes( CVS.toJsrAmount( US, "Self-raising Flour", ONE_QUART) ), is(500d));
+        assertThat( toGrammes( CVS.toJsrAmount( US, "Wholewheat Flour", ONE_QUART) ), is(480d));
+        assertThat( toGrammes( CVS.toJsrAmount( US, "Rolled Oats", ONE_QUART) ), is(340d));
+    }
+
+    @Test
+    public void testFlour_OZ() {
+        assertThat( toGrammes( CVS.toJsrAmount( US, "Plain Flour", /* Fluid */ ONE_OZ) ), is(15.624999999999998));
+        assertThat( toGrammes( CVS.toJsrAmount( US, "Bread Flour", /* Fluid */ ONE_OZ) ), is(15.874999999999998));
+    }
+
+    /**
+     * Helpers
+     */
+    private double toGrammes( final Optional<Amount<javax.measure.quantity.Quantity>> amount) {
+    	return amount.get().to( SI.KILOGRAM.divide(1000) ).getEstimatedValue();
+    }
+
+    private double toMl( final Optional<Amount<javax.measure.quantity.Quantity>> amount) {
+    	return amount.get().to( NonSI.LITRE.divide(1000) ).getEstimatedValue();
     }
 }
