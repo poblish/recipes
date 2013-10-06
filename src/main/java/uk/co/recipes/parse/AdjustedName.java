f2f7160a2b0c945970d7fc5635e0bd6e0df07814
Andrew Regan <aregan@gmail.com>
2013-10-06 01:33:34 +0100

    Add toString() etc.

--- src/main/java/uk/co/recipes/parse/AdjustedName.java
+++ src/main/java/uk/co/recipes/parse/AdjustedName.java
@@ -7,6 +7,8 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.util.Collection;
 
+import com.google.common.base.Objects;
+
 
 /**
  * TODO
@@ -33,5 +35,29 @@ public class AdjustedName {
 
     public Collection<String> getNotes() {
         return notesToAdd;
+    }
+
+    @Override
+	public int hashCode() {
+		return Objects.hashCode( adjustedName, notesToAdd);
+	}
+
+	@Override
+	public boolean equals( final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof AdjustedName)) {
+			return false;
+		}
+		final AdjustedName other = (AdjustedName) obj;
+		return Objects.equal( adjustedName, other.adjustedName) && Objects.equal( notesToAdd, other.notesToAdd);
+	}
+
+	public String toString() {
+    	return Objects.toStringHelper(this).add( "name", adjustedName).add( "notes", notesToAdd).toString();
     }
 }
