ed4e985e72e114c20cee5f26b1b4454dbe46324b
Andrew Regan <aregan@gmail.com>
2013-10-11 00:52:38 +0100

    New findPartialWithTags() + route + refactor

--- web/recipes-web/app/controllers/Search.java
+++ web/recipes-web/app/controllers/Search.java
@@ -56,18 +56,32 @@ public class Search extends Controller {
     public Result findPartial() throws IOException {
         final String[] searchString = request().queryString().get("q");
         final String[] count = request().queryString().get("count");
-        final String[] callback = request().queryString().get("callback");
         // System.out.println("x = " + Arrays.toString(searchString) + " / " + Arrays.toString(count) + " / " + Arrays.toString(callback));
 
         final List<ISearchResult<?>> results = search.findPartial( searchString[0], Integer.parseInt( count[0] ));  // FIXME Use 20 if not set, or set > 20
+
+    	return returnSearchResults( results, request().queryString().get("callback"));
+    }
+
+    public Result findPartialWithTags() throws IOException {
+        final String[] searchString = request().queryString().get("q");
+        final String[] count = request().queryString().get("count");
+        // System.out.println("x = " + Arrays.toString(searchString) + " / " + Arrays.toString(count) + " / " + Arrays.toString(callback));
+
+        final List<ISearchResult<?>> results = search.findPartialWithTags( searchString[0], Integer.parseInt( count[0] ));  // FIXME Use 20 if not set, or set > 20
+
+    	return returnSearchResults( results, request().queryString().get("callback"));
+    }
+
+    private Result returnSearchResults( final List<ISearchResult<?>> inResults, final String[] inCallback) throws IOException {
     	// System.out.println("results = " + mapper.writeValueAsString(results));
 
         // Play 2.1.x doesn't support Jackson 2.x, so we can't pass JsonNode
 
-    	if ( callback.length > 0) {
-    		return ok( callback[0] + "(" + mapper.writeValueAsString(results) + ")").as("application/json");
+    	if ( inCallback.length > 0) {
+    		return ok( inCallback[0] + "(" + mapper.writeValueAsString(inResults) + ")").as("application/json");
     	}
 
-    	return ok( mapper.writeValueAsString(results) ).as("application/json");
+    	return ok( mapper.writeValueAsString(inResults) ).as("application/json");
     }
 }
