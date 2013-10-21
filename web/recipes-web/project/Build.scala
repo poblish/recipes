f4e7d1b162054b150712e31778a7a215b621c345
Andrew Regan <aregan@gmail.com>
2013-10-21 13:03:43 +0100

    Merge branch 'Play-2.2.0'

--- web/recipes-web/project/Build.scala
+++ web/recipes-web/project/Build.scala
@@ -4,17 +4,19 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
+  val scalaVersion    = "2.10.2"
+
   val appName         = "recipes-web"
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
     javaCore,
-    "uk.co.recipes" % "recipes-domain" % "0.0.1-SNAPSHOT",
+    "uk.co.recipes" % "recipes-domain" % "0.0.1-SNAPSHOT" exclude("org.scala-stm", "scala-stm_2.10.0"),
     "com.squareup.dagger" % "dagger-compiler" % "1.1.0",
     "com.codahale.metrics" % "metrics-core" % "3.0.1",
     "com.codahale.metrics" % "metrics-servlets" % "3.0.1",
-    "com.feth" %% "play-authenticate" % "0.3.0-SNAPSHOT"
+    "com.feth" %% "play-authenticate" % "0.3.0-SNAPSHOT" exclude("org.scala-stm", "scala-stm_2.10.0")
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
