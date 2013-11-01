32d3f8b7ff5e7666ad752ebb2d255ed1fdc39163
Andrew Regan <aregan@gmail.com>
2013-11-01 00:31:17 +0000

    Add MetricRegistry and CuisineColours to Tags controller

--- web/recipes-web/app/controllers/Tags.java
+++ web/recipes-web/app/controllers/Tags.java
@@ -7,13 +7,18 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import com.codahale.metrics.MetricRegistry;
+
 import play.mvc.Controller;
 import play.mvc.Result;
+import service.PlayAuthUserServicePlugin;
 import uk.co.recipes.api.ICanonicalItem;
 import uk.co.recipes.api.ITag;
+import uk.co.recipes.api.IUser;
 import uk.co.recipes.service.api.ISearchAPI;
 import uk.co.recipes.service.impl.EsSearchService;
 import uk.co.recipes.tags.TagUtils;
+import uk.co.recipes.ui.CuisineColours;
 
 /**
  * 
@@ -25,15 +30,25 @@ import uk.co.recipes.tags.TagUtils;
 public class Tags extends Controller {
 
 	private ISearchAPI search;
+	private MetricRegistry metrics;
+	private CuisineColours colours;
 
     @Inject
-    public Tags( final EsSearchService inSearch) {
+    public Tags( final EsSearchService inSearch, final MetricRegistry inMetrics, final CuisineColours inColours) {
         this.search = checkNotNull(inSearch);
+        this.metrics = checkNotNull(inMetrics);
+        this.colours = checkNotNull(inColours);
     }
 
     public Result display( final String name) throws IOException {
+        final IUser user1 = getLocalUser();
+
         final ITag theTag = TagUtils.forName( name.replace( ' ', '_').toUpperCase() );
         final List<ICanonicalItem> results = search.findItemsByTag(theTag);
-        return ok(views.html.tags.render( theTag, results));
+        return ok(views.html.tags.render( user1, colours, theTag, results, search.findRandomRecipesByTag( 9, theTag)));
+    }
+
+    private IUser getLocalUser() {
+        return /* Yuk! */ PlayAuthUserServicePlugin.getLocalUser( metrics, session());
     }
 }
