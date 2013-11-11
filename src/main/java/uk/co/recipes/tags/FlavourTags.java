88f049a985dff88878be4292045cdcaab4c0d2a7
Andrew Regan <aregan@gmail.com>
2013-11-11 23:59:21 +0000

    Slightly reluctantly (re-) introduce 'Mustardy' flavour

--- src/main/java/uk/co/recipes/tags/FlavourTags.java
+++ src/main/java/uk/co/recipes/tags/FlavourTags.java
@@ -16,7 +16,7 @@ public enum FlavourTags implements ITag {
 	ANISEED, SWEET, SOUR, ORANGE, LEMON, LIME, ALMOND, COFFEE, GARLIC,
 
 	// Categories from 'The Flavour Thesaurus'
-	FLORAL, EARTHY, TART, TANNIC, SWEET_NUTTY, UMAMI, /* Already got White Fish */ SMOKY_SALTY, SUGAR_SWEET, CREAMY, SPICY /* Diff'ate from 'SPICE' */, WARM, HOT, PEPPERY, DARK_RICH, GRASSY, CITRUS /* Originally 'CITRUSSY' */
+	FLORAL, EARTHY, TART, TANNIC, SWEET_NUTTY, UMAMI, /* Already got White Fish */ SMOKY_SALTY, SUGAR_SWEET, CREAMY, SPICY /* Diff'ate from 'SPICE' */, WARM, HOT, PEPPERY, MUSTARDY, DARK_RICH, GRASSY, CITRUS /* Originally 'CITRUSSY' */
 
 	;
 
