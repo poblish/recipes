5510c60e3a01d041effd10ddb99fa4e1ddcd3f3c
Andrew Regan <aregan@gmail.com>
2013-11-18 12:40:34 +0000

    Myrrix ingester: start to support Fave and Block requests

--- src/main/java/uk/co/recipes/myrrix/MyrrixPrefsIngester.java
+++ src/main/java/uk/co/recipes/myrrix/MyrrixPrefsIngester.java
@@ -3,6 +3,9 @@
  */
 package uk.co.recipes.myrrix;
 
+import com.google.common.base.Objects;
+import com.google.common.base.Throwables;
+import com.google.common.io.Files;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
@@ -10,19 +13,12 @@ import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-
 import javax.inject.Inject;
-
 import net.myrrix.client.ClientRecommender;
-
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.yaml.snakeyaml.Yaml;
-
+import uk.co.recipes.api.ICanonicalItem;
 import uk.co.recipes.persistence.EsItemFactory;
-
-import com.google.common.base.Objects;
-import com.google.common.base.Throwables;
-import com.google.common.io.Files;
 
 /**
  * TODO
@@ -38,33 +34,85 @@ public class MyrrixPrefsIngester {
 	public String parseRecommendations( final File inFile) throws IOException {
 		final StringBuilder sb = new StringBuilder();
 
-		for ( Object eachDoc : new Yaml().loadAll( Files.toString( inFile, Charset.forName("utf-8")))) {
+		visitRecommendationsFile( inFile, new UserPrefsVisitor() {
 
-			@SuppressWarnings("unchecked")
-			final Map<Integer,List<Map<String,Object>>> eachDocMap = (Map<Integer,List<Map<String,Object>>>) eachDoc;
+            @Override
+            public void visit( int userId, Map<String, Object> eachUserPref) throws IOException {
+                if ( eachUserPref.containsKey("block") ||
+                    ( /* All done, don't create '1' rating */ eachUserPref.containsKey("fave") && !eachUserPref.containsKey("score"))) {
+                    return;
+                }
 
-			for ( Entry<Integer,List<Map<String,Object>>> eachUserDoc : eachDocMap.entrySet()) {
-				final int userId =  eachUserDoc.getKey();
+                ////////////////////////////////  Deal with ratings/scores
 
-				for ( Map<String,Object> eachUserPref : eachUserDoc.getValue()) {
-					if ( sb.length() > 0) {
-						sb.append('\r');
-					}
+                if ( sb.length() > 0) {
+                    sb.append('\r');
+                }
 
-					try {
-						sb.append(userId).append(',').append( itemFactory.get((String) eachUserPref.get("i") ).get().getId() ).append(',').append( Objects.firstNonNull( eachUserPref.get("score"), "1"));
-					}
-					catch (IOException e) {
-						System.err.println("IGNORE: " + e);
-					}
-				}
-			}
-		}
+                final ICanonicalItem item = itemFactory.get((String) eachUserPref.get("i") ).get();
+                sb.append(userId).append(',').append( item.getId() ).append(',').append( Objects.firstNonNull( eachUserPref.get("score"), "1"));
+            }}
+		);
 
 		System.out.println("Parsed: " + sb.toString().replace( '\r', ' ') + "  (" + sb.length() + " chars)");
 
 		return sb.toString();
 	}
+
+    public String parseFaves( final File inFile) throws IOException {
+        visitRecommendationsFile( inFile, new UserPrefsVisitor() {
+
+            @Override
+            public void visit( int userId, Map<String, Object> eachUserPref) throws IOException {
+                if (eachUserPref.containsKey("block")) {
+                    return;
+                }
+
+                if (eachUserPref.containsKey("fave")) {
+                    final ICanonicalItem item = itemFactory.get((String) eachUserPref.get("i") ).get();
+                    System.out.println("Fave: " + item);
+                }
+            }}
+        );
+
+        return "ok";
+    }
+
+    public String parseBlocks( final File inFile) throws IOException {
+        visitRecommendationsFile( inFile, new UserPrefsVisitor() {
+
+            @Override
+            public void visit( int userId, Map<String, Object> eachUserPref) throws IOException {
+                if (eachUserPref.containsKey("block")) {
+                    final ICanonicalItem item = itemFactory.get((String) eachUserPref.get("i") ).get();
+                    System.out.println("BLOCK: " + item);
+                }
+            }}
+        );
+
+        return "ok";
+    }
+
+    private void visitRecommendationsFile( final File inFile, final UserPrefsVisitor inVisitor) throws IOException {
+        for ( Object eachDoc : new Yaml().loadAll( Files.toString( inFile, Charset.forName("utf-8")))) {
+
+            @SuppressWarnings("unchecked")
+            final Map<Integer,List<Map<String,Object>>> eachDocMap = (Map<Integer,List<Map<String,Object>>>) eachDoc;
+
+            for ( Entry<Integer,List<Map<String,Object>>> eachUserDoc : eachDocMap.entrySet()) {
+                final int userId =  eachUserDoc.getKey();
+
+                for ( Map<String,Object> eachUserPref : eachUserDoc.getValue()) {
+                    try {
+                        inVisitor.visit( userId, eachUserPref);
+                    }
+                    catch (IOException e) {
+                        System.err.println("IGNORE: " + e);
+                    }
+                }
+            }
+        }
+    }
 
 	public void ingestRecommendations( final String inData) throws IOException {
 		try {
@@ -74,5 +122,9 @@ public class MyrrixPrefsIngester {
 		catch (TasteException e) {
 			Throwables.propagate(e);
 		}
+	}
+
+	private static interface UserPrefsVisitor {
+	    void visit( final int userId, final Map<String,Object> eachUserPref) throws IOException;
 	}
 }
--- src/test/java/uk/co/recipes/taste/MyrrixPrefsIngestTest.java
+++ src/test/java/uk/co/recipes/taste/MyrrixPrefsIngestTest.java
@@ -3,26 +3,22 @@
  */
 package uk.co.recipes.taste;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-
+import dagger.Module;
+import dagger.ObjectGraph;
+import dagger.Provides;
 import java.io.File;
 import java.io.IOException;
-
 import javax.inject.Inject;
 import javax.inject.Singleton;
-
 import org.apache.http.client.ClientProtocolException;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import uk.co.recipes.DaggerModule;
 import uk.co.recipes.mocks.MockFactories;
 import uk.co.recipes.myrrix.MyrrixPrefsIngester;
 import uk.co.recipes.persistence.EsItemFactory;
-import dagger.Module;
-import dagger.ObjectGraph;
-import dagger.Provides;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 /**
  * TODO
@@ -39,11 +35,23 @@ public class MyrrixPrefsIngestTest {
         ObjectGraph.create( new TestModule() ).inject(this);
 	}
 
-	@Test
-	public void testLoading() throws IOException {
-		final String contents = ingester.parseRecommendations( new File("src/test/resources/recommendations1.yaml") );
-		assertThat( contents, is("2,1510281992,1\r2,461905405,3\r2,467117389,10\r2,2133018664,10\r2,632006359,8\r2,2360810,5\r3,2044784730,8\r3,1859661278,5\r3,779718024,1\r3,2066500,3"));
-	}
+    @Test
+    public void testLoadScores() throws IOException {
+        final String contents = ingester.parseRecommendations( new File("src/test/resources/recommendations1.yaml") );
+        assertThat( contents, is("2,1510281992,1\r2,461905405,3\r2,467117389,10\r2,2133018664,10\r2,632006359,8\r2,2360810,5\r3,2044784730,8\r3,1859661278,5\r3,779718024,1\r3,2066500,3"));
+    }
+
+    @Test
+    public void testLoadFaves() throws IOException {
+        final String contents = ingester.parseFaves( new File("src/test/resources/recommendations1.yaml") );
+        assertThat( contents, is("ok"));
+    }
+
+    @Test
+    public void testLoadBlocks() throws IOException {
+        final String contents = ingester.parseBlocks( new File("src/test/resources/recommendations1.yaml") );
+        assertThat( contents, is("ok"));
+    }
 
     @Module( includes=DaggerModule.class, overrides=true, injects=MyrrixPrefsIngestTest.class)
     static class TestModule {
