31c19011b7054effa5b41f9764795cb4f5bb6cb5
Andrew Regan <aregan@gmail.com>
2017-08-02 00:11:13 +0100

    Latest

--- runMyrrix.sh
+++ runMyrrix.sh
@@ -1,5 +1,14 @@
 pkill -f net.myrrix.web.Runner
 pkill -f org.elasticsearch.bootstrap.ElasticSearch
 
+mvn clean install -DskipTests=true
+mvn -f myrrixRescorer/ clean install  -DskipTests=true
+
+elasticsearch-0.90.6/bin/elasticsearch
+
 nohup java -Dmodel.features=6 -Dmodel.als.lambda=1.03 -cp "/Users/andrewregan/Development/java/recipe_explorer/myrrix-serving-1.0.1.jar:/Users/andrewregan/.m2/repository/uk/co/recipes/myrrix-rescorer/0.0.1-SNAPSHOT/myrrix-rescorer-0.0.1-SNAPSHOT.jar" net.myrrix.web.Runner --port 8080 --rescorerProviderClass=uk.co.recipes.myrrix.RecipesRescorer > /private/tmp/nohup.out 2>&1&
-nohup elasticsearch -f -D es.config=/usr/local/Cellar/elasticsearch/0.90.6/config/elasticsearch.yml     > /private/tmp/nohup.out 2>&1&
+# nohup elasticsearch -f -D es.config=/usr/local/Cellar/elasticsearch/0.90.6/config/elasticsearch.yml     > /private/tmp/nohup.out 2>&1&
+
+
+# cd web/recipes-web
+# ../../play-2.1.5/play .
